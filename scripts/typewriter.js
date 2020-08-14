1dbad41db1d60380e9142ad0e249b06d490f03fd
LeoCarrilho <66269764+LeoCarrilho@users.noreply.github.com>
2020-08-14 17:39:54 -0300

    Ignição

---
+++ scripts/typewriter.js
@@ -0,0 +1,11 @@
+function typeWritter(texto, idElemento, tempo) {
+    var char = texto.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) return clearInterval(typer);
+        var next = char.pop();
+        document.getElementById(idElemento).innerHTML += next;
+    }, tempo);
+}
+
+
+window.onload = typeWritter('Consultoria e Treinamentos', 'escreva', 100);
